e77cdbcf2f00200569ce9d425eb406ae4f48f477
Kenneth Belitzky <kenny@belitzky.com>
2009-09-22 21:34:17 -0300

    Refs. #424 - modified strutures for attributes.

--- pinax/templatetags/templatetags/theme_tags.py
+++ pinax/templatetags/templatetags/theme_tags.py
@@ -9,22 +9,31 @@ class SilkNode(template.Node):
 
     def __init__(self, name, attrs):
         self.name = template.Variable(name)
-        self.attrs = [template.Variable(attr) for attr in attrs]
+        self.attrs = {}
+        for attr in attrs:
+            key, value = attr.split("=")
+            self.attrs[key] = template.Variable(value)
+                
 
     def render(self, context):
         """Render the img tag with specified attributes"""
 
-        joined_attrs = '' 
+        joined_attrs = ''
 
         try:
             name = self.name.resolve(context)
-            joined_attrs = ' '.join([var.resolve(context) for var in self.attrs])
+            joined_attrs = self._join_attrs(context)
         except template.VariableDoesNotExist:
             print ""
 
         return """<img src="%spinax/images/silk/icons/%s.png" %s />""" % (settings.STATIC_URL,
                                                                           name,
                                                                           joined_attrs)
+
+    def _join_attrs(self, context):
+        attrs = ['%s="%s"' % (k, v.resolve(context)) for k, v in self.attrs.items()]
+        return ' '.join(attrs)
+
 
 @register.tag
 def silk(parser, token):
@@ -33,10 +42,8 @@ def silk(parser, token):
     Usage {{ silk "image_name" arg1="value1" arg2="value2" ... }}
      
     """
-    try:
-        split_token = token.split_contents()
-    except ValueError:
-        raise template.TemplateSyntaxError, "%r tag has argument errors" % token.split_contents()[0]
-    
+
+    split_token = token.split_contents()
+
     #send name and attributes to SilkNode
     return SilkNode(split_token[1], split_token[2:])
