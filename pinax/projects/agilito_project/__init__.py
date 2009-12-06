60bccc16f05a1ebcdf1ba9cb71b13f20e0fa7be6
Kenneth Belitzky <kenny@belitzky.com>
2009-12-06 17:57:26 -0200

    initial pinax/agilito integration

---
+++ pinax/projects/agilito_project/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__about__ = """
+This project integrates agilito and pinax. It is intended to be the starting point for things like code project
+management where each code project gets its own wiki, task tracking system
+and threaded discussions.
+"""
